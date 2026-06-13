fa01bc5041dbad05760362451d07e435807f2afe
choiseulgi <kcmcom8282@gmail.com>
2026-06-13 17:25:17 +0900

    fix: 페이지 이동 시 스크롤 최상단으로 초기화 (ScrollToTop)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import AboutMe from './pages/AboutMe'
 import Projects from './pages/Projects'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => { window.scrollTo(0, 0) }, [pathname])
+  return null
+}
+
 export default function App() {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
